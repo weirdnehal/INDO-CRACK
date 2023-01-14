408253ab78951daf93499bb5251299bf3663b349
Nehal Ahmed <107056647+weirdnehal@users.noreply.github.com>
2023-01-14 16:58:48 +0600

    Update INDO_CRACK.py

--- INDO_CRACK.py
+++ INDO_CRACK.py
@@ -1,6 +1,6 @@
-# Encrypted by Hamid Meer
-# Whatsapp 03155912839 
-# Github- https://github.com/Hamii-king-06
+# Author By Nehal Ahmed
+# Whatsapp  +8801613016943
+# Github- https://github.com/weirdnehal
 
 exec("".join(map(chr,[int("".join(str({'😀': 0, '😁': 3, '😂': 6, '😃': 1, '😄': 2, '😅': 4, '😉': 7, '😊': 8, '😛': 9, '🤣': 5}[i]) for i in x.split())) for x in
 "😁 🤣  😁 😁  😅 😉  😃 😃 😉  😃 😃 🤣  😃 😃 😅  😅 😉  😛 😊  😃 😀 🤣  😃 😃 😀  😅 😉  😃 😃 😄\
